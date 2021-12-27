0f20a46f6134d7a4edcc8c7a713b5e3ce9956192
pdsuwwz <pdsu.wwz@foxmail.com>
2021-12-27 23:06:49 +0800

    🍍 chore: eslint upgrade

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,11 +13,11 @@ module.exports = {
     withDefaults: 'readonly'
   },
   extends: [
+    'eslint:recommended',
     'plugin:vue/base',
     'plugin:vue/vue3-essential',
     'plugin:vue/vue3-strongly-recommended',
-    'plugin:vue/vue3-recommended',
-    '@vue/standard'
+    'plugin:vue/vue3-recommended'
   ],
   parserOptions: {
     ecmaFeatures: {
@@ -28,7 +28,9 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-unused-vars': 1,
+
+    // vue
+    'vue/multi-word-component-names': 0,
     'vue/no-unused-components': 1,
     'vue/no-mutating-props': 0,
     'vue/script-setup-uses-vars': 'error',
@@ -40,6 +42,40 @@ module.exports = {
       },
       svg: 'always',
       math: 'always'
-    }]
+    }],
+
+    // js
+    'no-unused-vars': 1,
+    'no-undef': 1,
+    // https://eslint.org/docs/rules/no-var
+    'no-var': 'error',
+    // https://cn.eslint.org/docs/rules/no-trailing-spaces
+    'no-trailing-spaces': 2, // 禁用行尾空白
+    'comma-style': ['error', 'last'],
+    'comma-dangle': ['error', 'never'],
+    'no-irregular-whitespace': 2,
+    'no-multi-spaces': 1,
+    'no-multiple-empty-lines': [
+      2,
+      {
+        max: 1
+      }
+    ],
+    // https://cn.eslint.org/docs/rules/eol-last
+    'eol-last': 2,
+    quotes: [
+      'error',
+      'single',
+      {
+        avoidEscape: true,
+        allowTemplateLiterals: true
+      }
+    ],
+    // https://eslint.org/docs/rules/prefer-const
+    'prefer-const': 2,
+    camelcase: ['error', { properties: 'never' }],
+    indent: ['error', 2, { SwitchCase: 1 }],
+    semi: ['error', 'never'],
+    'space-before-function-paren': 'error'
   }
 }
